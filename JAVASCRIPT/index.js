beb963ad12432a5ab5a0b690df96acb2160d5a2f
adityagupta0251 <itsokkadi0251@gmail.com>
2025-08-26 11:53:06 +0530

    added HTML & CSS

---
+++ JAVASCRIPT/index.js
@@ -0,0 +1,21 @@
+function sum(a){
+    let number = 0;
+
+    for (let i = 1; i <= a; i++){
+        number += i;
+
+
+
+
+
+    }
+    return number;
+
+}
+
+let num = prompt("Enter a number: ");
+let result = sum(num);
+console.log(result);
+
+
+console.log(num);
