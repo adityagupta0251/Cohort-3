b3bca15a611d5d3345e6c23d51fc25d6d2f69fa2
adityagupta0251 <itsokkadi0251@gmail.com>
2025-08-26 12:14:09 +0530

    completed data types

---
+++ JAVASCRIPT/datatypes.js
@@ -0,0 +1,10 @@
+let number  = 42;
+let string  = "Hello, World!";
+let isMale = true;
+
+let arraysofnum = [1,2,3,4, 5];
+
+
+console.log("Hey number : " + number + " " + string + " Your gender is : Male ? =>  " + isMale + " "+ "Your account balance is : " + arraysofnum[0]);
+
+// Completed
