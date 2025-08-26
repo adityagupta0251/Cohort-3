dbc8f4380f30610b29482e4a26238d972b3ee06d
adityagupta0251 <itsokkadi0251@gmail.com>
2025-08-26 12:29:32 +0530

    completed operators & functions with added assignment

--- JAVASCRIPT/functions.js
+++ JAVASCRIPT/functions.js
@@ -8,3 +8,39 @@ function greet (names){
 let myName = "Aditya";
 let callthisfunc = greet(myName);
 
+// Assignment 1
+// Write a function sum that finds the sum of two numbers. 
+// Side quest - Try passing in a string instead of a number and see what happens?
+
+//solution
+function sum (a, b){
+    let sumof = a +b;
+    console.log("The sum of " + a + " and " + b + " is : " + sumof);
+    return sumof;
+}
+
+let value = sum(34, 45);
+
+// Completed
+
+
+// Assignment 2
+// Write a function called canVote that returns true or false if the age of a user is > 18
+
+function canVote (age){
+    if(age >= 18){
+        console.log("You can vote");
+        return true;
+    }else{
+        console.log("You cannot vote");
+        return false;
+    }
+
+}
+
+let myage = 29;
+let mysecondage = 16;
+let ifcanvote = canVote(myage);
+let ifcanvote2 = canVote(mysecondage);
+
+// Completed
