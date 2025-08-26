4f1025c7070f47179236742ea469681f562251a3
adityagupta0251 <itsokkadi0251@gmail.com>
2025-08-26 12:39:47 +0530

    completed if /else statement with assignments

---
+++ JAVASCRIPT/ifelse.js
@@ -0,0 +1,23 @@
+let age = 19;
+if(age > 18){
+    console.log("Your are an adult");
+
+
+}else{
+    console.log("Your are a children");
+}
+
+// Assignment: 1
+// Write an if/else statement that checks if a number is even or odd. If it's even, print "The number is even." Otherwise, print "The number is odd."
+
+let mynumber = 7;
+if(mynumber %2 === 0){
+    console.log("The number is even. ");
+
+}else{
+    console.log("The number is odd.");
+
+}
+
+// Try to change the value of mynumber to test both conditions. try with even & odd 
+// Completed 
