bd56f09e8eaa184701d7d48a69941d9638cd81f2
adityagupta0251 <itsokkadi0251@gmail.com>
2025-08-26 12:18:53 +0530

    completed data types

---
+++ JAVASCRIPT/operators.js
@@ -0,0 +1,11 @@
+let sum = 10 + 5; // Arithmetic operator
+let isequalsto = (10 === sum); // comparison operator
+let istrue = (5 < 10) && (10 > 5); // Logical AND operator
+
+
+console.log("The sum is : " + sum);
+console.log("Is 10 equal to sum? : " + isequalsto);
+console.log("Is 5 is less than 10 AND 10 is greater than 5? : " + istrue);
+
+
+// Completed
